94e7bb7ad299e4ca6fae6b6463c7233490040271
Sosthèn <sosthen.gaillard@gmail.com>
2019-01-29 17:33:13 +0100

    Add composite aggregation

---
+++ lib/Elastica/Aggregation/Composite.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Elastica\Aggregation;
+
+use Elastica\Exception\InvalidException;
+
+/**
+ * Class Composite.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-composite-aggregation.html
+ */
+class Composite extends AbstractAggregation
+{
+    /**
+     * @var array Aggregations used as sources for the composite
+     */
+    protected $_sources = [];
+
+    /**
+     * Sets the amount of composite buckets to be returned.
+     *
+     * @param int $size The amount of composite buckets to be returned.
+     *
+     * @return $this
+     */
+    public function setSize($size)
+    {
+        return $this->setParam('size', $size);
+    }
+
+    /**
+     * Adds a source
+     *
+     * @param AbstractAggregation $aggregation The aggregation to add as source
+     *
+     * @return $this
+     */
+    public function addSource($aggregation)
+    {
+        if (!($aggregation instanceof Histogram) && !($aggregation instanceof Terms)) {
+            throw new \InvalidArgumentException('The composite aggregations only supports histograms, date-histograms and terms aggregations as sources');
+        }
+        $this->_sources[] = $aggregation;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        if (count($this->_sources) === 0) {
+            throw new InvalidException(
+                'At least one source should be set'
+            );
+        }
+        $array = parent::toArray();
+
+        $baseName = $this->_getBaseName();
+
+        $array[$baseName]['sources'] = $this->_convertArrayable($this->_sources);
+
+        return $array;
+    }
+}
